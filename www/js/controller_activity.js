2b700f4d0e93dcede30dca973348db0fd7e81816
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-11-25 15:58:04 +0800

    [1125] ng-repeat裡面不能用ng-model #16 target_view.html #17 target_edit.html #39 target_recommend close #14 profile.html close #15 target_set.html #33 controller_target.js

---
+++ www/js/controller_activity.js
@@ -0,0 +1,7 @@
+var controller_activity = function ($scope) {
+    $scope.ctl_activity = {};
+    
+    $scope.ctl_activity.enter_from_target = function () {
+        $.console_trace("完成設定目標後，移動到此頁面");
+    };
+};
