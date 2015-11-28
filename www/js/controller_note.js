b99475a670545cd25e5ba7e15dcdbda2e90f194b
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-11-28 16:02:31 +0800

    #19 1128 準備開始 close #56

--- www/js/controller_note.js
+++ www/js/controller_note.js
@@ -1,27 +1,17 @@
 var controller_note = function ($scope) {
     
-    $scope.note = {
-        my: "",
-        other: []
-    };
+    var _ctl = {};
+    
+    var _log_file = "controller_note.js";
     
     // ------------------------------
     
-    $scope.ctl_note = {};
-    
-    $scope.ctl_note.enter = function () {
-        $scope.ctl_note.init(function () {
-            app.navi.pushPage("note.html");
-        });
-        
-        $scope.ctl_note.load_other_note();
-    };
-     
-    $scope.ctl_note.init = function (_callback) {
-        $.trigger_callback(_callback);
+    _ctl.enter = function () {
+        app.navi.pushPage("note.html");
     };
     
-    $scope.ctl_note.load_other_note = function (_callback) {
-        $.trigger_callback(_callback);
-    };
+    // -------------------------------
+    
+    $scope.ctl_note = _ctl;
+    
 };
