a3e659c7dda148fe14d07a8520163b9f0067cd27
USER <USER@USER-PC>
2015-11-29 11:57:52 +0800

    close #23 1129 commit

--- www/js/app_ready.js
+++ www/js/app_ready.js
@@ -3,23 +3,25 @@
 var _app_ready = function ($scope) {
     ons.ready(function () {
 
-        $scope.set_swipeable(false);
+//        $scope.set_swipeable(false);
         
         // 如果要刪除profile，則執行這個
         //$scope.profile_reset();
         
-        $scope.flashcard_setup(function () {
-            $scope.db_profile.load_from_db(function () {
-                if ($scope.profile === undefined) {
-                    $scope.profile = $scope.profile_mock;
-                    app.navi.replacePage("profile.html", {animation: 'none'});
-                }
-                else {
-                    //$scope.ctl_target.enter_from_profile(false);
-                    $scope.set_swipeable(true);
-                    app.navi.replacePage("learn_flashcard.html", {animation: 'none'});
-                }
-            });
-        });
+        app.navi.replacePage("menu.html", {animation: 'none'});
+        
+//        $scope.flashcard_setup(function () {
+//            $scope.db_profile.load_from_db(function () {
+//                if ($scope.profile === undefined) {
+//                    $scope.profile = $scope.profile_mock;
+//                    app.navi.replacePage("profile.html", {animation: 'none'});
+//                }
+//                else {
+//                    //$scope.ctl_target.enter_from_profile(false);
+//                    $scope.set_swipeable(true);
+//                    app.navi.replacePage("learn_flashcard.html", {animation: 'none'});
+//                }
+//            });
+//        });
     });
 };
