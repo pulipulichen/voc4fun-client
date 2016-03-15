305ef3e9cadd4b15312a587d249961c7cdd330f8
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-03-15 10:29:59 +0800

    0315 準備佈置實驗組

--- git-scripts/clone.sh
+++ git-scripts/clone.sh
@@ -1,6 +1,6 @@
-if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=origin/clt; fi
-if [ -z "$KALS_PATH" ]; then KALS_PATH=/var/www/html/clt/voc4fun-client; fi
-if [ -z "$KALS_DIR" ]; then KALS_DIR=/var/www/html/clt; fi
+if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=origin/ctl; fi
+if [ -z "$KALS_PATH" ]; then KALS_PATH=/var/www/html/ctl/voc4fun-client; fi
+if [ -z "$KALS_DIR" ]; then KALS_DIR=/var/www/html/ctl; fi
 
 #echo $KALS_DIR
 
