e63bc1a248d2857e421fed829da41a03a3c1317f
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-03-15 10:15:38 +0800

    0315 準備ctl

--- git-scripts/clone.sh
+++ git-scripts/clone.sh
@@ -1,9 +1,9 @@
-if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=voc4fun-server/master; fi
-if [ -z "$KALS_PATH" ]; then KALS_PATH=/var/www/html/voc4fun-client; fi
-if [ -z "$KALS_DIR" ]; then KALS_DIR=/var/www/html; fi
+if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=origin/clt; fi
+if [ -z "$KALS_PATH" ]; then KALS_PATH=/var/www/html/ctl/voc4fun-client; fi
+if [ -z "$KALS_DIR" ]; then KALS_DIR=/var/www/html/ctl; fi
 
 #echo $KALS_DIR
 
 #cd $KALS_DIR
 git clone git://github.com/pulipulichen/voc4fun-client.git "$KALS_PATH"
-git reset --hard origin/master
+git reset --hard "$KALS_BRANCH"
