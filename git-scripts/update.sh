f3abd4822b79af722a9782729897a2b5ebb528b5
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-04-14 16:35:34 +0800

    0412 修改uuid

--- git-scripts/update.sh
+++ git-scripts/update.sh
@@ -1,4 +1,4 @@
-if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=origin/master; fi
+if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=voc4fun-server/ctl; fi
 if [ -z "$KALS_DIR" ]; then KALS_DIR=/var/www/html; fi
 if [ -z "$KALS_PATH" ]; then KALS_PATH=/var/www/html/voc4fun-client; fi
 
