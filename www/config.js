83f5081cb7950e1bb5413de670ca8f8352cb8005
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-10-25 17:34:54 +0800

    Update config.js

--- www/config.js
+++ www/config.js
@@ -25,13 +25,7 @@ var app_config = function ($scope) {
         /**
          * 伺服器的位置
          */
-        "server_url": "http://exp-voc4fun-2015.dlll.nccu.edu.tw/voc4fun-server/",
-        
-        //"server_url": "http://192.168.11.50/voc4fun/voc4fun-server/",
-        //"server_url": "http://pc.pulipuli.info/voc4fun/voc4fun-server/",
-        // http://pc.pulipuli.info:8080/voc4fun/voc4fun-client  http://goo.gl/BCV6gy
-        //"server_url": "/voc4fun/voc4fun-server/",
-        
+        "server_url": "http://localhost/voc4fun-server/",
         
         /**
          * 單字卡的位置
